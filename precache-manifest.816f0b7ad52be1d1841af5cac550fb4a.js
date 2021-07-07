b3cec56cc9140aedef831df476d2c99d30578db3
Avinash Tadavarthy <avinash.tadavarthy@gmail.com>
2021-07-07 15:25:12 +0530

    gh-pages

---
+++ precache-manifest.816f0b7ad52be1d1841af5cac550fb4a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba2a325f26362be9509496dd1e26eb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e7eae806e86d26cdd16",
+    "url": "/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "977cd985582f7f6650c1",
+    "url": "/static/css/main.bca5736e.chunk.css"
+  },
+  {
+    "revision": "0e7eae806e86d26cdd16",
+    "url": "/static/js/2.d1f6fc71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d1f6fc71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "977cd985582f7f6650c1",
+    "url": "/static/js/main.be50349a.chunk.js"
+  },
+  {
+    "revision": "0188671cffff4c633daf",
+    "url": "/static/js/runtime-main.77264515.js"
+  },
+  {
+    "revision": "be3649419e0e399e723726f2a0ef2e69",
+    "url": "/static/media/24 crafts.be364941.png"
+  },
+  {
+    "revision": "83945991f281d264ddc2578abc8e90b3",
+    "url": "/static/media/Avinash-Tadavarthy-Resume.83945991.pdf"
+  },
+  {
+    "revision": "9798db787cf127454a83c84844ebba09",
+    "url": "/static/media/arrow-down.9798db78.svg"
+  },
+  {
+    "revision": "3de7c6441dbc01a3e8be516de82afc1f",
+    "url": "/static/media/avinash-tadavarthy-white.3de7c644.svg"
+  },
+  {
+    "revision": "9c70486a99a7bf5a728d9038cbea2863",
+    "url": "/static/media/avinash-tadavarthy.9c70486a.svg"
+  },
+  {
+    "revision": "f89cbe0e2773d33723c3d5c4a4fb250b",
+    "url": "/static/media/avinew.f89cbe0e.png"
+  },
+  {
+    "revision": "d540fa751485151a807ea6b3993cffdd",
+    "url": "/static/media/bingo.d540fa75.svg"
+  },
+  {
+    "revision": "ceefc2b37e9ce39f79b80b98698dd6a2",
+    "url": "/static/media/compress.ceefc2b3.svg"
+  },
+  {
+    "revision": "951cca30d2e632c11daa958885e720d9",
+    "url": "/static/media/dance1.951cca30.png"
+  },
+  {
+    "revision": "bf1003b8708badd1d8dd1b44298fec3a",
+    "url": "/static/media/dance2.bf1003b8.png"
+  },
+  {
+    "revision": "846471aa255f6d1b014834a0b90013ec",
+    "url": "/static/media/fidelity.846471aa.jpg"
+  },
+  {
+    "revision": "1d81baf76ba9ee849fe493b4d9ced0d7",
+    "url": "/static/media/github.1d81baf7.svg"
+  },
+  {
+    "revision": "3c269ffd56af2dae8d432a91b1d41b99",
+    "url": "/static/media/image (a2).3c269ffd.png"
+  },
+  {
+    "revision": "63046f1072f4052dcdc1fe160cdf750a",
+    "url": "/static/media/ioscalc.63046f10.jpg"
+  },
+  {
+    "revision": "7dad44bff6cdd059e1400fe7dfb26b8f",
+    "url": "/static/media/matchezy.7dad44bf.png"
+  },
+  {
+    "revision": "8b9226d4e9e36c69983d860cf2efb4be",
+    "url": "/static/media/name.8b9226d4.svg"
+  },
+  {
+    "revision": "494ad9e61abffa36d89ea875508200ce",
+    "url": "/static/media/needl-shirt.494ad9e6.png"
+  },
+  {
+    "revision": "d357bc31cda047e206c451f89c405de4",
+    "url": "/static/media/needl-wireframes.d357bc31.png"
+  },
+  {
+    "revision": "0e94882db58be6e084590aa37f12ca0e",
+    "url": "/static/media/needl.0e94882d.png"
+  },
+  {
+    "revision": "7b72c04272dce6720a2b1b86e281ff72",
+    "url": "/static/media/node.7b72c042.svg"
+  },
+  {
+    "revision": "9ca0d402d14683bb9eb77d22b8d975a0",
+    "url": "/static/media/play.9ca0d402.svg"
+  },
+  {
+    "revision": "f1b84300048bbadcacca8519ea16e99e",
+    "url": "/static/media/portfolio.f1b84300.svg"
+  },
+  {
+    "revision": "9c554fd389f6ed069a999eeea5595e1c",
+    "url": "/static/media/pythoncolor.9c554fd3.jpg"
+  },
+  {
+    "revision": "725190afd794fb846be1e107d1bf6e01",
+    "url": "/static/media/quicksort.725190af.jpg"
+  },
+  {
+    "revision": "b659216971637d1718045ab9f660812d",
+    "url": "/static/media/robot.b6592169.svg"
+  },
+  {
+    "revision": "fe00882d051f29170ced85378038721d",
+    "url": "/static/media/svac.fe00882d.jpg"
+  },
+  {
+    "revision": "65a6b2f905a5ce9e674ff71f38607c82",
+    "url": "/static/media/ticket.65a6b2f9.svg"
+  },
+  {
+    "revision": "837517755c014c09d8343da592ebe882",
+    "url": "/static/media/under-construction.83751775.webp"
+  },
+  {
+    "revision": "3df78ffba6c17fe9f3b7021b87800005",
+    "url": "/static/media/vasavi.3df78ffb.png"
+  },
+  {
+    "revision": "c8ffb1c990e9d36345de99efc863037f",
+    "url": "/static/media/zoho.c8ffb1c9.png"
+  }
+]);
